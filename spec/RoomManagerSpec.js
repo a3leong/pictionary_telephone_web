2a496949177c74280de8362832645e20f88c511a
Aaron Leong <a3leong@gmail.com>
2016-10-18 16:51:23 -0700

    Created roommanager class and passed written tests

--- spec/RoomManagerSpec.js
+++ spec/RoomManagerSpec.js
@@ -9,49 +9,32 @@ describe("RoomManagerTests", function() {
   });
 
   it("Should be able to make unique game rooms",function() {
-    // Make game room http
-    var  roomId = gameRoom.getId();
-    expect(roomId).toBeTruthy();
+    var idList = {};
+    for(var i=0;i<100;i++) {
+      var newId = roomManager.createRoom();
+      expect(idList[newId]).toBeFalsy(); // Check if it already exists
+      idList[roomManager.createRoom()] = true; // If it doesn't fail expect we do this
+    }
   });
 
-  it("Should be able to view the info of a room and set variables", function() {
-    // Check for defaults
-    expect(gameRoom.getPhraseTime()).toEqual(Config.defaultPhraseTime);
-    expect(gameRoom.getDrawTime()).toEqual(Config.defaultDrawTime);
-
-    gameRoom.setPhraseTime(gameRoom.getPhraseTime()+3);
-    gameRoom.setDrawTime(gameRoom.getDrawTime()-3);
-    expect(gameRoom.getPhraseTime()).toEqual(Config.defaultPhraseTime+3);
-    expect(gameRoom.getDrawTime()).toEqual(Config.defaultDrawTime-3);
+  it("Should be able to remove a room, throws error if doesn't exist", function() {
+    var id = roomManager.createRoom();
+    roomManager.destroyRoom(id); // Set to null or undefined
+    expect(roomManager.containsRoom(id)).toBeFalsy();
+    expect(function(){roomManager.destroyRoom(id);}).toThrowError(Err.ROOM_DNE);
   });
 
-  it("Should expect an error when setting variables outside of config boundaries", function() {
-    expect(function(){gameRoom.setPhraseTime(Config.minPhraseTime-1);}).toThrowError(Err.SET_PHASE_TIME_BOUND);
-    expect(function(){gameRoom.setPhraseTime(Config.maxPhraseTime+1);}).toThrowError(Err.SET_PHASE_TIME_BOUND);
-    expect(function(){gameRoom.setDrawTime(Config.minDrawTime-1);}).toThrowError(Err.SET_DRAW_TIME_BOUND);
-    expect(function(){gameRoom.setDrawTime(Config.maxDrawTime+1);}).toThrowError(Err.SET_DRAW_TIME_BOUND);
-  });
-
-  it("Should be able to add a player, but throw error if player already exists and throw error if socket is the same", function() {
+  it("Should throw an error trying to add player from a nonexistant room", function() {
     var socketSpy = jasmine.createSpy("Socketspy");
-    var socketSpy2 = jasmine.createSpy("Socketspy2");
-    gameRoom.addPlayer(socketSpy,"Player1");
-    expect(gameRoom.containsPlayer("Player1")).toBeTruthy();
-    expect(function() {gameRoom.addPlayer(socketSpy2, "Player1");}).toThrowError(Err.PLAYER_ID_EXISTS);
-    expect(function() {gameRoom.addPlayer(socketSpy, "Player2");}).toThrowError(Err.CONNECTION_EXISTS);
+    expect(function(){roomManager.addPlayer("nonexistantRoom", "fakePlayerId", socketSpy);}).toThrowError(Err.ROOM_DNE);
   });
 
-  it("Should be able to remove a player, but throw error if DNE", function() {
-    var socketSpy1 = jasmine.createSpy("Socketspy");
-    socketSpy1.close = function(){};
-    var socketSpy2 = jasmine.createSpy("Socketspy");
-    socketSpy2.close = function(){};
-    gameRoom.addPlayer(socketSpy1, "Player1");
-    gameRoom.addPlayer(socketSpy2, "Player2");
-    gameRoom.removePlayer("Player1");
+  it("Should throw an error trying to remove player from nonexistant room", function() {
+    expect(function(){roomManager.removePlayer("nonexistantRoom", "fakePlayerId");}).toThrowError(Err.ROOM_DNE);
+  });
 
-    expect(gameRoom.containsPlayer("Player1")).toBeFalsy();
-    expect(gameRoom.containsPlayer("Player2")).toBeTruthy();
-    expect(function() {gameRoom.removePlayer("Player1");}).toThrowError(Err.PLAYER_ID_DNE);
+  it("Should throw an error trying to get a socketPool from a nonexistant room", function() {
+    expect(function(){roomManager.getPlayerPool("nonexistantRoom");}).toThrowError(Err.ROOM_DNE);
+
   });
 });
