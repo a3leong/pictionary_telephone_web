5dff8dec785dfee1a1ca7f86c9d172e0bb531fb3
wesmelon <wesklau@gmail.com>
2016-11-28 23:14:06 -0800

    Aaron's name generator

--- client/src/Lobby.js
+++ client/src/Lobby.js
@@ -5,7 +5,31 @@ import {Card, CardHeader, CardActions, CardText} from 'material-ui/Card';
 
 import './Lobby.css';
 
+const names = [
+  'Wes',
+  'Sarah',
+  'Kevin',
+  'Jason',
+  'Bernard',
+  'Ruby',
+  'Nadine',
+  'Clement',
+  'Hannah',
+  'Gilia',
+];
+
 class Lobby extends Component {
+  constructor(props) {
+    super(props);
+    const randy = Math.floor((Math.random() * 1000) + 1);
+    const bottle = Math.floor((Math.random() * names.length));
+    const selected = names[bottle] + "x" + randy;
+    
+    this.state = {
+      name: selected,
+    };
+  }
+
   render() {
     return (
       <div className="Lobby">
@@ -13,11 +37,12 @@ class Lobby extends Component {
           <CardHeader
             title="Telephone Pictionary"
             subtitle="Create or join a game"
-            style={{"width": "60vw"}}
+            style={{"width": "40vw"}}
           />
           <CardText>
             <TextField
               hintText="Your Name"
+              defaultValue={this.state.name}
             />
           </CardText>
           <CardActions>
