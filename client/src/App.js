6f0c6644378a7be2210d077dd1551861f155bfd7
wesmelon <wesklau@gmail.com>
2016-11-28 22:40:41 -0800

    Change routing to fit game flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
 
 import './App.css';
 
@@ -21,16 +17,16 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("/hello", {
-      accept: 'application/json',
-    }).then((response) => {
-      return response.json();
-    }).then((response) => {
-      this.setState({
-        open: false,
-        payload: response
-      });
-    });
+    // fetch("/hello", {
+    //   accept: 'application/json',
+    // }).then((response) => {
+    //   return response.json();
+    // }).then((response) => {
+    //   this.setState({
+    //     open: false,
+    //     payload: response
+    //   });
+    // });
   }
 
   handleToggle = () => this.setState({open: !this.state.open});
@@ -39,22 +35,7 @@ class App extends Component {
     return (
       <MuiThemeProvider>
         <div className="App">
-          <AppBar
-            title="Telephone Pictionary"
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-            onLeftIconButtonTouchTap={this.handleToggle}
-          />
           {this.props.children}
-
-          <Drawer
-            docked={false}
-            className="App"
-            open={this.state.open}
-            onRequestChange={(open) => this.setState({open})}
-          >
-            <MenuItem containerElement={<Link to="/lobby" />}>Lobby</MenuItem>
-            <MenuItem containerElement={<Link to="/drawboard" />}>Drawboard</MenuItem>
-          </Drawer>
         </div>
       </MuiThemeProvider>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import App from './App';
 import Lobby from './Lobby';
 import DrawBoard from './DrawBoard';
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <Route path="lobby" component={Lobby} />
+      <IndexRoute component={Lobby} />
       <Route path="drawboard" component={DrawBoard} />
     </Route>
   </Router>,
